ac67fdd3d7adea9c8e0ca9ba7364d17ddbd51b7d
haa <haa>
2020-03-18 09:07:37 +0100

    Empty spring boot project with h2 and springWeb

---
+++ src/main/java/com/zuehlke/haa/grails2spring/grails2spring/Grails2springApplication.java
@@ -0,0 +1,13 @@
+package com.zuehlke.haa.grails2spring.grails2spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grails2springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grails2springApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuehlke/haa/grails2spring/grails2spring/Grails2springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuehlke.haa.grails2spring.grails2spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grails2springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
